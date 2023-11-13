e7f63249807a8d02d76f629cc9058742d19102e3
bob <1498833800@qq.com>
2023-11-13 10:22:34 +0800

    update

--- src/Https/request.tsx
+++ src/Https/request.tsx
@@ -1,6 +1,6 @@
-import axios, { AxiosRequestConfig } from 'axios';
-import { PendingType } from './type';
-import { getSession, removeSession } from '@/utils/base';
+import axios, { AxiosRequestConfig } from "axios";
+import { PendingType } from "./type";
+import { clearSession, getSession, removeSession } from "@/utils/base";
 // 取消重复请求
 const pending: Array<PendingType> = [];
 const CancelToken = axios.CancelToken;
@@ -8,7 +8,7 @@ const CancelToken = axios.CancelToken;
 // axios 实例
 const instance = axios.create({
   timeout: 10000,
-  responseType: 'json'
+  responseType: "json",
 });
 
 // 移除重复请求
@@ -17,9 +17,14 @@ const removePending = (config: AxiosRequestConfig) => {
     const item: number = +key;
     const list: PendingType = pending[key];
     // 当前请求在数组中存在时执行函数体
-    if (list.url === config.url && list.method === config.method && JSON.stringify(list.params) === JSON.stringify(config.params) && JSON.stringify(list.data) === JSON.stringify(config.data)) {
+    if (
+      list.url === config.url &&
+      list.method === config.method &&
+      JSON.stringify(list.params) === JSON.stringify(config.params) &&
+      JSON.stringify(list.data) === JSON.stringify(config.data)
+    ) {
       // 执行取消操作
-      list.cancel('操作太频繁，请稍后再试');
+      list.cancel("操作太频繁，请稍后再试");
       // 从数组中移除记录
       pending.splice(item, 1);
     }
@@ -31,9 +36,15 @@ instance.interceptors.request.use(
   (request: any) => {
     // removePending(request);
     request.cancelToken = new CancelToken((c) => {
-      pending.push({ url: request.url, method: request.method, params: request.params, data: request.data, cancel: c });
+      pending.push({
+        url: request.url,
+        method: request.method,
+        params: request.params,
+        data: request.data,
+        cancel: c,
+      });
     });
-    request.headers.token = getSession('token')
+    request.headers.token = getSession("token");
     return request;
   },
   (error: any) => {
@@ -43,8 +54,11 @@ instance.interceptors.request.use(
 // 添加响应拦截器
 instance.interceptors.response.use(
   (response: any) => {
+    if (response.data.code === 401) {
+      return clearSession();
+    }
     // removePending(response.config);
-    return response
+    return response;
   },
   (error: any) => {
     const response = error.response;
@@ -52,5 +66,4 @@ instance.interceptors.response.use(
   }
 );
 
-
-export default instance
+export default instance;
